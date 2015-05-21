0b199beef66f11ae4384300bcd6ed27e8a6da5cc
Rodolfo Ribeiro Barreto <rodolfobarretoweb@gmail.com>
2015-05-21 14:25:00 -0300

    Changed the name of some properties, past the base directory name of the new file to the constructor and indented better some properties

--- PhpPdfFiller.php
+++ PhpPdfFiller.php
@@ -1,45 +1,52 @@
 <?php
 final Class PhpPdfFiller {
   private $pdftk_path;
+  private $new_file_directory;
 
-  public function __construct($pdftk_path = "/usr/local/bin/pdftk"){
-    $this->pdftk_path = $pdftk_path;
+  public function __construct($pdftk_path = '/usr/local/bin/pdftk', $new_file_directory = '/tmp/'){
+    $this->pdftk_path         = $pdftk_path;
+    $this->new_file_directory = $new_file_directory;
   }
 
-  private function createFDF($file, $info){
-    $data = "%FDF-1.2\n%âãÏÓ\n1 0 obj\n<< \n/FDF << /Fields [ ";
+  private function createFDF($base_file, $data){
+    $code = "%FDF-1.2\n%âãÏÓ\n1 0 obj\n<< \n/FDF << /Fields [ ";
 
-    foreach($info as $field => $value) {
+    foreach($data as $field => $value) {
       if(is_array($value)) {
-        $data .= '<</T('.$field.')/V[';
+        $code .= '<</T('.$field.')/V[';
 
         foreach($value as $opt) {
-          $data .= '('.trim($opt).')';
+          $code .= '('.trim($opt).')';
         }
 
-        $data .= ']>>';
+        $code .= ']>>';
       } else {
-         $data .= '<</T('.$field.')/V('.trim($value).')>>';
+         $code .= '<</T('.$field.')/V('.trim($value).')>>';
       }
     }
 
-    $data .= "] \n/F (" . $file . ") /ID [ <" . md5(time()) . ">\n] >>";
-    $data .= " \n>> \nendobj\ntrailer\n";
-    $data .= "<<\n/Root 1 0 R \n\n>>\n%%EOF\n";
+    $code .= "] \n/F (" . $base_file . ") /ID [ <" . md5(time()) . ">\n] >>";
+    $code .= " \n>> \nendobj\ntrailer\n";
+    $code .= "<<\n/Root 1 0 R \n\n>>\n%%EOF\n";
 
-    return $data;
+    return $code;
   }
 
-  public function writeFile(array $params = array()) {
-    $fdf_data = $this->createFDF($params['file'], $params['info']);
+  public function writeFile($new_file_name, $base_file, $data) {
+    $new_file = $this->new_file_directory . $new_file_name;
 
-    $file = fopen($params['dir_file'] . $params['file_name'] . '.fdf', 'w');
+    $fdf_file = $this->createFDF($base_file, $data);
+    $tmp_file = fopen($new_file . '.fdf', 'w');
 
-    if($file && fwrite($file, $fdf_data, strlen($fdf_data))) {
-      passthru("{$this->pdftk_path} A='{$params['file']}' fill_form '{$params['file_name']}.fdf' output '{$params['file_name']}.pdf' drop_xfa need_appearances");
+    if($tmp_file && fwrite($tmp_file, $fdf_file, strlen($fdf_file))) {
+      $command  = "{$this->pdftk_path} A='{$base_file}' ";
+      $command .= "fill_form '{$new_file}.fdf' ";
+      $command .= "output '{$new_file}.pdf' drop_xfa need_appearances";
+
+      passthru($command);
 
       header('Content-type: application/pdf');
-      echo file_get_contents("{$params['file_name']}.pdf");
+      echo file_get_contents("{$new_file}.pdf");
     }
 
     fclose($file);
